978fbbd1f1ec320654cd0431e33970c2e6136dfa
mikeheilmann <mike.heilmann.nstg@gmail.com>
2022-05-14 01:35:29 +0200

    init

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let startKnopf=document.getElementById("startKnopf");
 startKnopf.addEventListener("click",startenSpiel);
 
 function startenSpiel(){
-    console.log("sssssssssss");
+    
 }
 
 function weiterFrage (){
