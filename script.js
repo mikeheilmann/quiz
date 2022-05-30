7c4fbe291f7b94b7ee0e9617c058c06c630584fb
mikeheilmann <mike.heilmann.nstg@gmail.com>
2022-05-30 17:41:32 +0200

    init

--- script.js
+++ script.js
@@ -11,14 +11,30 @@ function weiterFrage(){
 } 
 
 function beendenSpiel(){
-
 }
 
 function zeigeFrage(){
-    
 }
 
 function waehleAntwort(){
-
 }
+
+let fragen=[
+    {
+        frage: "Was ist 1+1 ?",
+        antwort: {
+            a: "2",
+            b: "11"
+        },
+        richtigeAntwort: "a"
+    },
+    {
+        frage: "Was ist 2*2 ?",
+        antwort: {
+            a: "4",
+            b: "22"
+        },
+        richtigeAntwort: "a"
+    }
+];
 
